cf5a577b61abda22f0af5cd54dc1f322a4233947
jion_kozono <zeon66190329@gmail.com>
2023-11-21 17:21:01 +0900

    remove: name input

--- js/app.js
+++ js/app.js
@@ -6,34 +6,25 @@ const defaultMessage = {
 document.addEventListener('DOMContentLoaded', function () {
   document.getElementById("saveBtn").addEventListener("click", saveSettings);
   // HTMLが読み込まれたら初期値を設定
-  chrome.storage.sync.get(['discordWebhookURL', 'name', 'comingMessage', 'leavingMessage'], function(items) {
+  chrome.storage.sync.get(['discordWebhookURL', 'comingMessage', 'leavingMessage'], function(items) {
       document.getElementById('webhookUrl').value = items.discordWebhookURL || '';
-      document.getElementById('yourName').value = items.name || '';
       document.getElementById('comingMessage').value = items.comingMessage || defaultMessage.defaultComingMessage;
       document.getElementById('leavingMessage').value = items.leavingMessage || defaultMessage.defaultLeavingMessage;
   });
 });
 
 function saveSettings() {
-  var webhookUrl = document.getElementById('webhookUrl').value;
-  var yourName = document.getElementById('yourName').value;
-  var comingMessage = document.getElementById('comingMessage').value;
-  var leavingMessage = document.getElementById('leavingMessage').value;
+  const webhookUrl = document.getElementById('webhookUrl').value;
+  const comingMessage = document.getElementById('comingMessage').value;
+  const leavingMessage = document.getElementById('leavingMessage').value;
 
-  if (!webhookUrl || !yourName){
-    if (!webhookUrl && !yourName){
-      alert('Please enter a Webhook URL for Discord and your name');
-    }else if (!webhookUrl){
-      alert('Please enter a Webhook URL');
-    }else{
-      alert('Please enter your name');
-    }
+  if (!webhookUrl){
+    alert('Please enter a Webhook URL');
     return
   }
   // 保存後、必要であれば再度初期値を設定する
   chrome.storage.sync.set({
       discordWebhookURL: webhookUrl,
-      name: yourName,
       comingMessage: comingMessage,
       leavingMessage: leavingMessage
   }, function () {
--- js/script.js
+++ js/script.js
@@ -2,6 +2,9 @@ import { defaultMessage } from "./const";
 
 const channelName = "discord";
 
+let isComingButtonDisabled = false;
+let isLeavingButtonDisabled = true;
+
 async function getChromeStorageData() {
   const items = await chrome.storage.sync.get([
     "discordWebhookURL",
@@ -19,33 +22,62 @@ async function getChromeStorageData() {
 
 // KOTのタイムレコーダーページでイベントリスナーを設定
 function setEventListener() {
-  const coming_elem = Array.from(document.getElementsByClassName("record-btn-inner record-clock-in"));
-  if (coming_elem[0]) {
-    coming_elem[0].addEventListener(
+  const comingElem = document.querySelector(".record-btn-inner.record-clock-in");
+  const leavingElem = document.querySelector(".record-btn-inner.record-clock-out");
+
+  if (comingElem) {
+    comingElem.addEventListener(
       "click",
       async function () {
-        await send_attendance_report(1);
+        if (!isComingButtonDisabled) {
+          isComingButtonDisabled = true;
+          isLeavingButtonDisabled = false;
+          await sendAttendanceReport(1);
+          // ボタンの無効化処理
+          comingElem.style.pointerEvents = "none";
+          comingElem.style.background = "#8e8e8e";
+          console.log({disabled: comingElem.style.pointerEvents})
+          // 退勤ボタンの活性化
+          leavingElem.style.pointerEvents = "auto";
+        }
       },
       false
     );
   }
 
-  const leaving_elem = Array.from(document.getElementsByClassName("record-btn-inner record-clock-out"));
-  if (leaving_elem[0]) {
-    leaving_elem[0].addEventListener(
+  if (leavingElem) {
+    leavingElem.addEventListener(
       "click",
       async function () {
-        await send_attendance_report(0);
+        if (!isLeavingButtonDisabled) {
+          isLeavingButtonDisabled = true;
+          isComingButtonDisabled = false;
+          await sendAttendanceReport(0);
+          // ボタンの無効化処理
+          leavingElem.style.pointerEvents = "none";
+          leavingElem.style.background = "#8e8e8e";
+          removeClass(comingElem, ["record-btn-inner-hover", "record-clock-out-inner-hover"])
+          // 出勤ボタンの活性化
+          comingElem.style.pointerEvents = "auto";
+        }
       },
       false
     );
+  }
+  // 退勤ボタンは初期時押せない
+  if(isLeavingButtonDisabled){
+    leavingElem.style.pointerEvents = "none";
+    leavingElem.style.background = "#8e8e8e";
+  }else{
+    leavingElem.style.pointerEvents = "auto";
+    leavingElem.style.background = undefined;
   }
   window.alert(`打刻すると指定した${channelName}チャネルに勤怠報告が送信されます。`);
 }
 
 setTimeout(setEventListener, 1000);
 
-async function send_attendance_report(status) {
+async function sendAttendanceReport(status) {
   const { discordWebhookURL, name, comingMessage, leavingMessage } = await getChromeStorageData();
   if (!discordWebhookURL) {
     window.alert(`
@@ -74,4 +106,12 @@ async function send_attendance_report(status) {
       window.alert("メッセージを正常に送信できませんでした。");
     }
   });
+}
+
+function removeClass(element, classNames) {
+  if (element.classList) {
+    classNames.forEach(className => {
+      element.classList.remove(className);
+    });
+  }
 }
