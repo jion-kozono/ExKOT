63137cf7df4d58aa37fc8e08d9da39ac1fe3d516
jion_kozono <zeon66190329@gmail.com>
2023-12-29 10:18:04 +0900

    fix: button style

--- src/ts/components/Button.ts
+++ src/ts/components/Button.ts
@@ -1,8 +1,8 @@
 export const setButtonStyle = (button: HTMLButtonElement, backgroundColor: string) => {
   button.style.position = "fixed";
-  button.style.top = "50%";
+  button.style.top = "60%";
   button.style.left = "50%";
-  button.style.transform = "translate(-50%, -50%)";
+  button.style.transform = "translate(-60%, -50%)";
   button.style.color = "#fff";
   button.style.backgroundColor = backgroundColor;
   button.style.borderRadius = "4px";
